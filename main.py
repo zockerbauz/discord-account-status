f3700a5e609044cd67802079b278322500ab91bb
zockerbauz <148658884+zockerbauz@users.noreply.github.com>
2024-03-09 11:11:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ headers = {
     'authority': 'discord.com',
     'accept': '*/*',
     'accept-language': 'de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7',
-    'authorization': 'process.env.token',
+    'authorization': token,
     'content-type': 'application/json',
     'cookie': '__dcfduid=ca1c8ba0d6e411eebb63f3acec168b78; __sdcfduid=ca1c8ba1d6e411eebb63f3acec168b78d61a959e63226577ab9d4948b2d620cb953c1cfa5d553b94d6448bbf67317cd7; _ga=GA1.1.315153795.1709203299; _ga_YL03HBJY7E=GS1.1.1709203299.1.1.1709203309.0.0.0; __stripe_mid=55ddfd73-fac1-4f0b-8089-45bfe1117322959324; __cfruid=25f63b084fc32f60f69ca2a63131e601671af714-1709976920; _cfuvid=GC_lJcQN3xtUenSiwrWsuOX1GuzXBc7Le4I282.68xA-1709976920939-0.0.1.1-604800000; OptanonConsent=isIABGlobal=false&datestamp=Sat+Mar+09+2024+10%3A35%3A48+GMT%2B0100+(Mitteleurop%C3%A4ische+Normalzeit)&version=6.33.0&hosts=&landingPath=https%3A%2F%2Fdiscord.com%2F&groups=C0001%3A1%2CC0002%3A0%2CC0003%3A0; locale=de; cf_clearance=1mqpemgdSwLStrm08zWdkVYht0PzggBxZ5OJ15pkH08-1709976930-1.0.1.1-lkc7K__Evj2cGbR3HwS00YPGllknh_0xG9TWkKrbIjj8ClhHwzDEQV3egtus_4Q9BFNf69wyAXPnQScB0gIW1A',
     'origin': 'https://discord.com',
